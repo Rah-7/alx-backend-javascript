7567e36ed9cc1998838357789e3e308038c36a5c
Rah-7 <rahigirmam16@gmail.com>
2023-09-26 20:32:58 +0300

    edit file

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,25 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup () {
-  const photoPromise = uploadPhoto();
-  // use then on it to check if photoPromise is correctly resolved
-  return photoPromise.then((result) => {
-    const photoBody = result.body;
-    return photoBody;
-  }).then((photoBody) => {
-    /** then use the body (profile photo gotten from the)
-   previous function */
-    const userPromise = createUser();
-    userPromise.then((userResult) => {
-      /** initialising first and last names from the createUser returned/resolved object.
-         * apparently, i shouldnt use dot notation bc of eslint
-         * if not it'll have been: const firstName = userResult.firstName;
-         */
-      const { firstName } = userResult;
-      const { lastName } = userResult;
-      console.log(`${photoBody} ${firstName} ${lastName}`);
-    });
-  }).catch(() => {
-    console.log('Signup system offline');
-  });
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
