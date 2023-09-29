da14af3a8316434d8d86447d96eb2db65c69ca28
Rah-7 <rahigirmam16@gmail.com>
2023-09-29 18:25:29 +0300

    edit file

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,7 +5,11 @@ export default class Car {
     this._color = color;
   }
 
-  static clone (car) {
-    return new car.constructor(car._brand, car._motor, car._color);
+  cloneCar () {
+    /** return new instance of whatever class is referenced by the 'this'
+     * e.g const newcar = new Car(brand, motor, color)
+     * anothercar = newcar.cloneCar(); will return newcar's class object bc of 'this.constructor' */
+    const clonedCar = new this.constructor(this._brand, this._motor, this._color);
+    return clonedCar;
   }
 }
