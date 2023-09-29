17868a38776ae7689cbb0ca9aae630c110dbb5d3
Rah-7 <rahigirmam16@gmail.com>
2023-09-29 17:58:41 +0300

    currency

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,42 +1,30 @@
 export default class Currency {
   constructor (code, name) {
-    if (typeof code === 'string') {
-      this._code = code;
-    } else {
-      throw new TypeError('Code must be a string');
-    }
-    if (typeof name === 'string') {
-      this._name = name;
-    } else {
-      throw new TypeError('Name must be a string');
-    }
-  }
-
-  set name (name) {
-    if (typeof name === 'string') {
-      this._name = name;
-    } else {
-      throw new TypeError('Name must be a string');
-    }
-  }
-
-  get name () {
-    return this._name;
-  }
-
-  set code (code) {
-    if (typeof code === 'string') {
-      this._code = code;
-    } else {
-      throw new TypeError('Code must be a string');
-    }
+    this.code = code;
+    this.name = name;
   }
 
   get code () {
     return this._code;
   }
 
+  set code (value) {
+    this._code = value;
+  }
+
+  get name () {
+    return this._name;
+  }
+
+  set name (value) {
+    this._name = value;
+  }
+
+  /**
+   * Creates the full string representation of this Currency.
+   * @returns {String}
+   */
   displayFullCurrency () {
-    return `${this._name} (${this._code})`;
+    return `${this.name} (${this.code})`;
   }
 }
