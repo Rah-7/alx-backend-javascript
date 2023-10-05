7c3bb059f24e8584a833ea7696f84492c53d77b1
Rah-7 <rahigirmam16@gmail.com>
2023-10-05 19:34:47 +0300

    list

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,17 +1,16 @@
-export default function getListStudentIds(arg) {
-  if (Array.isArray(arg) === true) {
-    /** create a new map from the arg which is a list
-         * the map will be a map containing the ids
-         * since it must be set as key values, i am just setting the id as value for names
-         */
-    const returnedArray = [];
-    const individualsMap = new Map(
-      arg.map((object) => [object.firstName, object.id]),
-    );
-    for (const id of individualsMap.values()) {
-      returnedArray.push(id);
-    }
-    return returnedArray;
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns
+ */
+export default function getListStudentIds (students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
   }
   return [];
-
+}
